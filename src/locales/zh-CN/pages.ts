b29d8a70aa3b6f44ab92d6087b5eb4c0a78b19b7
knwins <knwin@msn.com>
2024-01-10 20:59:50 +0800

    update commit

--- src/locales/zh-CN/pages.ts
+++ src/locales/zh-CN/pages.ts
@@ -397,9 +397,9 @@ export default {
   'pages.store.username.search': '用户名搜索',
   'pages.store.address.placeholder': '请输入站点详细地址',
   'pages.store.address.required': '请输入站点详细地址',
-  'pages.store.latitude': '经度',
-  'pages.store.latitude.placeholder': '经度',
-  'pages.store.longitude': '纬度',
+  'pages.store.latitude': '纬度',
+  'pages.store.latitude.placeholder': '请输入纬度',
+  'pages.store.longitude': '经度',
   'pages.store.longitude.placeholder': '请输入经度',
 
   'pages.product.ids': '批量IDS',
