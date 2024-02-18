9cfc3e6fbed744ae75fdc93de10c82e6e4bb5574
knwins <knwin@msn.com>
2024-02-18 14:05:43 +0800

    update commit

--- src/pages/User/components/UserModel.tsx
+++ src/pages/User/components/UserModel.tsx
@@ -1,11 +1,14 @@
-import { StoreParams } from '@/pages/Operation/data';
-import { queryStoreSelect } from '@/pages/Operation/service';
-import { ModalForm, ProFormDigit, ProFormSelect, ProFormText } from '@ant-design/pro-form';
+import { StoreItem, StoreParams } from '@/pages/Operation/data';
+import { addStore, queryStoreSelect, updateStore } from '@/pages/Operation/service';
+import ProForm, { ModalForm, ProFormDigit, ProFormSelect, ProFormText } from '@ant-design/pro-form';
+import type { ActionType } from '@ant-design/pro-table';
 import { useIntl } from '@umijs/max';
+import { Button, message } from 'antd';
 import type { FC } from 'react';
+import { useRef, useState } from 'react';
 import { pagination, UserItem } from '../data';
 import styles from '../style.less';
-
+import AddressModel from './AddressModel';
 type UserModelProps = {
   done: boolean;
   visible: boolean;
@@ -15,13 +18,15 @@ type UserModelProps = {
 };
 
 const UserModel: FC<UserModelProps> = (props) => {
+  const actionRef = useRef<ActionType>();
   const { done, visible, current, onDone, onSubmit, children } = props;
+  const [addressVisible, setAddressVisible] = useState<boolean>(false);
   const intl = useIntl();
   if (!visible) {
     return null;
   }
 
-  const handleStoreSelect = async (type?: any,keywords?:any) => {
+  const handleStoreSelect = async (type?: any, keywords?: any) => {
     if (type === '') {
       return;
     }
@@ -32,7 +37,7 @@ const UserModel: FC<UserModelProps> = (props) => {
     };
     const options: StoreParams = {
       type: type,
-      keywords:keywords,
+      keywords: keywords,
     };
     //读取仓库数据
     const { data: storeData } = await queryStoreSelect({
@@ -54,41 +59,92 @@ const UserModel: FC<UserModelProps> = (props) => {
     return storeListOptions;
   };
 
+  const handleDone = () => {
+    setAddressVisible(false);
+  };
+
+  const handleAction = async (fields: StoreItem) => {
+    const loadingHidde = message.loading(
+      intl.formatMessage({
+        id: 'pages.tip.loading',
+      }),
+    );
+    loadingHidde();
+    try {
+      if (fields.id != null) {
+        fields.userId = fields.user.key;
+        const { success } = await updateStore({
+          ...fields,
+        });
+
+        if (success) {
+          message.success(
+            intl.formatMessage({
+              id: 'pages.tip.success',
+            }),
+          );
+          return true;
+        }
+      } else {
+        const { success } = await addStore({
+          ...fields,
+        });
+        if (success) {
+          message.success(
+            intl.formatMessage({
+              id: 'pages.tip.success',
+            }),
+          );
+          return true;
+        }
+      }
+      return false;
+    } catch (error) {
+      console.log(error);
+      message.error(
+        intl.formatMessage({
+          id: 'pages.tip.error',
+        }),
+      );
+      return false;
+    }
+  };
+
   //end
 
   return (
-    <ModalForm<UserItem>
-      visible={visible}
-      title={
-        done
-          ? null
-          : `${
-              current?.id
-                ? intl.formatMessage({
-                    id: 'pages.edit',
-                  })
-                : intl.formatMessage({
-                    id: 'pages.new',
-                  })
-            }`
-      }
-      className={styles.standardListForm}
-      width={640}
-      onFinish={async (values) => {
-        onSubmit(values);
-      }}
-      initialValues={current}
-      submitter={{
-        render: (_, dom) => (done ? null : dom),
-      }}
-      trigger={<>{children}</>}
-      modalProps={{
-        onCancel: () => onDone(),
-        destroyOnClose: true,
-        bodyStyle: done ? { padding: '72px 0' } : {},
-      }}
-    >
-      <>
+    <>
+      <ModalForm<UserItem>
+        visible={visible}
+        title={
+          done
+            ? null
+            : `${
+                current?.id
+                  ? intl.formatMessage({
+                      id: 'pages.edit',
+                    })
+                  : intl.formatMessage({
+                      id: 'pages.new',
+                    })
+              }`
+        }
+        className={styles.standardListForm}
+        width={640}
+        onFinish={async (values) => {
+          onSubmit(values);
+        }}
+        initialValues={current}
+        submitter={{
+          render: (_, dom) => (done ? null : dom),
+        }}
+        trigger={<>{children}</>}
+        modalProps={{
+          onCancel: () => onDone(),
+          destroyOnClose: true,
+          bodyStyle: done ? { padding: '72px 0' } : {},
+        }}
+      >
         <ProFormDigit name="id" hidden />
 
         <ProFormText
@@ -112,9 +168,7 @@ const UserModel: FC<UserModelProps> = (props) => {
 
         <ProFormText
           name="nick"
-          label={intl.formatMessage({
-            id: 'pages.user.nick',
-          })}
+          label="姓名"
           width="lg"
           rules={[
             {
@@ -150,23 +204,34 @@ const UserModel: FC<UserModelProps> = (props) => {
             id: 'pages.user.email.placeholder',
           })}
         />
-        <ProFormSelect
-          name="store"
-          width="lg"
-          showSearch
-          fieldProps={{
-            labelInValue: true,
-          }}
-          rules={[
-            {
-              required: true,
-            },
-          ]}
-          label="默认位置"
-          request={async (params) => {
-            return handleStoreSelect('ADDRESS',params.keyWords);
-          }}
-        />
+
+        <ProForm.Group title="用户地址">
+          <ProFormSelect
+            name="store"
+            width="md"
+            showSearch
+            fieldProps={{
+              labelInValue: true,
+            }}
+            rules={[
+              {
+                required: true,
+              },
+            ]}
+            request={async (params) => {
+              return handleStoreSelect('ADDRESS', params.keyWords);
+            }}
+          />
+          <Button
+            size="middle"
+            style={{ marginTop: '4px' }}
+            onClick={() => {
+              setAddressVisible(true);
+            }}
+          >
+            新增地址
+          </Button>
+        </ProForm.Group>
 
         <ProFormText
           name="description"
@@ -178,8 +243,23 @@ const UserModel: FC<UserModelProps> = (props) => {
             id: 'pages.user.description.placeholder',
           })}
         />
-      </>
-    </ModalForm>
+
+        <AddressModel
+          done={done}
+          visible={addressVisible}
+          onDone={handleDone}
+          onSubmit={async (value) => {
+            const success = await handleAction(value as StoreItem);
+            if (success) {
+              setAddressVisible(false);
+              if (actionRef.current) {
+                actionRef.current.reload();
+              }
+            }
+          }}
+        />
+      </ModalForm>
+    </>
   );
 };
 
